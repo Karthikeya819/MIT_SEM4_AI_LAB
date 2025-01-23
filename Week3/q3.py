12ac60d7372fe0fd0c7379a6f3b673a8c0ba0f77
Davasam Karthikeya <davasamkarthikeya@gmail.com>
2025-01-23 13:11:13 +0530

    update q3 of Week3

--- Week3/q3.py
+++ Week3/q3.py
@@ -1,22 +1,22 @@
 Maze_StartPoint = 1
 Maze_EndPoint = 4
 
-Maze = {0:[1,5],1:[0,2],2:[1,7],3:[4],4:[3,9],5:[0,10],6:[7],7:[2,6],8:[9,13],9:[4,8,14],10:[5,11],11:[10,16],12:[13],13:[8,12,18],14:[9,19],15:[16],16:[11,17],17:[16,18],18:[13,17],19:[14]}
+Maze = {0: [1, 5], 1: [0, 2], 2: [1, 7], 3: [4], 4: [3, 9], 5: [0, 10], 6: [7], 7: [2, 6], 8: [9, 13], 
+        9: [4, 8, 14], 10: [5, 11], 11: [10, 16], 12: [13], 13: [8, 12, 18], 14: [9, 19], 
+        15: [16], 16: [11, 15, 17], 17: [16, 18], 18: [13, 17], 19: [14]}
 
-def SearchMaze(Maze,Maze_StartPoint,Maze_EndPoint):
+def SearchMaze(Maze, Maze_StartPoint, Maze_EndPoint):
     VisitedNodes = []
     Stack = []
     Stack.append(Maze_StartPoint)
-    while len(Stack) != 0:
+    while Stack:
         vertex = Stack.pop()
-        if vertex == Maze_EndPoint:
-            print(VisitedNodes)
-            return
         if vertex not in VisitedNodes:
+            VisitedNodes.append(vertex)
+            if vertex == Maze_EndPoint:
+                print(VisitedNodes)
+                return
             for conn in Maze[vertex]:
                 Stack.append(conn)
-        VisitedNodes.append(vertex)
-        #print(VisitedNodes)
-        
 
-SearchMaze(Maze,Maze_StartPoint,Maze_EndPoint)
+SearchMaze(Maze, Maze_StartPoint, Maze_EndPoint)
